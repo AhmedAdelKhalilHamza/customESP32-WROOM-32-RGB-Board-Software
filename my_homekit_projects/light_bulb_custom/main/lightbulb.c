e7a53c6d70aac70ca28a83ef431d74363bdcb1fb
AhmedAdelKhalilHamza <ahmedadelkhalil99@gmail.com>
2025-11-01 19:04:16 +0200

    added RGB led brightness to lightbulb.c and connected it to LED on/off for homekit

--- my_homekit_projects/light_bulb_custom/main/lightbulb.c
+++ my_homekit_projects/light_bulb_custom/main/lightbulb.c
@@ -32,14 +32,33 @@
 #include <stdbool.h>
 #include "esp_log.h"
 #include "driver/gpio.h"
+#include "driver/ledc.h"
 
 static const char *TAG = "lightbulb";
 
-#define BLINK_GPIO_25 25
-#define BLINK_GPIO_26 26
-#define BLINK_GPIO_27 27
+#define LEDC_IO25_TIMER              LEDC_TIMER_0
+#define LEDC_IO25_MODE               LEDC_LOW_SPEED_MODE
+#define LEDC_IO25_OUTPUT_IO          (25) // Define the output GPIO
+#define LEDC_IO25_CHANNEL            LEDC_CHANNEL_0
+#define LEDC_IO25_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
+#define LEDC_IO25_FREQUENCY          (4000) // Frequency in Hertz. Set frequency at 4 kHz
+
+#define LEDC_IO26_TIMER              LEDC_TIMER_0
+#define LEDC_IO26_MODE               LEDC_LOW_SPEED_MODE
+#define LEDC_IO26_OUTPUT_IO          (26) // Define the output GPIO
+#define LEDC_IO26_CHANNEL            LEDC_CHANNEL_0
+#define LEDC_IO26_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
+#define LEDC_IO26_FREQUENCY          (4000) // Frequency in Hertz. Set frequency at 4 kHz
+
+#define LEDC_IO27_TIMER              LEDC_TIMER_0
+#define LEDC_IO27_MODE               LEDC_LOW_SPEED_MODE
+#define LEDC_IO27_OUTPUT_IO          (27) // Define the output GPIO
+#define LEDC_IO27_CHANNEL            LEDC_CHANNEL_0
+#define LEDC_IO27_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
+#define LEDC_IO27_FREQUENCY          (4000) // Frequency in Hertz. Set frequency at 4 kHz
 
 static uint8_t s_led_state = 0;
+uint32_t dimming_value = 0;
 
 /**
  * @brief initialize the lightbulb lowlevel module
@@ -47,13 +66,72 @@ static uint8_t s_led_state = 0;
 void lightbulb_init(void)
 {
     ESP_LOGI(TAG, "Dummy Light Driver Init.");
-    gpio_reset_pin(BLINK_GPIO_25);
-    gpio_reset_pin(BLINK_GPIO_26);
-    gpio_reset_pin(BLINK_GPIO_27);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO_25, GPIO_MODE_OUTPUT);
-    gpio_set_direction(BLINK_GPIO_26, GPIO_MODE_OUTPUT);
-    gpio_set_direction(BLINK_GPIO_27, GPIO_MODE_OUTPUT);
+
+    // Prepare and then apply the LEDC PWM timer configuration
+    ledc_timer_config_t ledc_IO25_timer = {
+        .speed_mode       = LEDC_IO25_MODE,
+        .duty_resolution  = LEDC_IO25_DUTY_RES,
+        .timer_num        = LEDC_IO25_TIMER,
+        .freq_hz          = LEDC_IO25_FREQUENCY,  // Set output frequency at 4 kHz
+        .clk_cfg          = LEDC_AUTO_CLK
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_IO25_timer));
+
+    // Prepare and then apply the LEDC PWM channel configuration
+    ledc_channel_config_t ledc_IO25_channel = {
+        .speed_mode     = LEDC_IO25_MODE,
+        .channel        = LEDC_IO25_CHANNEL,
+        .timer_sel      = LEDC_IO25_TIMER,
+        .intr_type      = LEDC_INTR_DISABLE,
+        .gpio_num       = LEDC_IO25_OUTPUT_IO,
+        .duty           = 0, // Set duty to 0%
+        .hpoint         = 0
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_IO25_channel));
+
+    // Prepare and then apply the LEDC PWM timer configuration
+    ledc_timer_config_t ledc_IO26_timer = {
+        .speed_mode       = LEDC_IO26_MODE,
+        .duty_resolution  = LEDC_IO26_DUTY_RES,
+        .timer_num        = LEDC_IO26_TIMER,
+        .freq_hz          = LEDC_IO26_FREQUENCY,  // Set output frequency at 4 kHz
+        .clk_cfg          = LEDC_AUTO_CLK
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_IO26_timer));
+
+    // Prepare and then apply the LEDC PWM channel configuration
+    ledc_channel_config_t ledc_IO26_channel = {
+        .speed_mode     = LEDC_IO26_MODE,
+        .channel        = LEDC_IO26_CHANNEL,
+        .timer_sel      = LEDC_IO26_TIMER,
+        .intr_type      = LEDC_INTR_DISABLE,
+        .gpio_num       = LEDC_IO26_OUTPUT_IO,
+        .duty           = 0, // Set duty to 0%
+        .hpoint         = 0
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_IO26_channel));
+
+    // Prepare and then apply the LEDC PWM timer configuration
+    ledc_timer_config_t ledc_IO27_timer = {
+        .speed_mode       = LEDC_IO27_MODE,
+        .duty_resolution  = LEDC_IO27_DUTY_RES,
+        .timer_num        = LEDC_IO27_TIMER,
+        .freq_hz          = LEDC_IO27_FREQUENCY,  // Set output frequency at 4 kHz
+        .clk_cfg          = LEDC_AUTO_CLK
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_IO27_timer));
+
+    // Prepare and then apply the LEDC PWM channel configuration
+    ledc_channel_config_t ledc_IO27_channel = {
+        .speed_mode     = LEDC_IO27_MODE,
+        .channel        = LEDC_IO27_CHANNEL,
+        .timer_sel      = LEDC_IO27_TIMER,
+        .intr_type      = LEDC_INTR_DISABLE,
+        .gpio_num       = LEDC_IO27_OUTPUT_IO,
+        .duty           = 0, // Set duty to 0%
+        .hpoint         = 0
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_IO27_channel));
 }
 
 /**
@@ -70,9 +148,18 @@ int lightbulb_set_on(bool value)
     {
         s_led_state = 0;
     }
-    gpio_set_level(BLINK_GPIO_25, s_led_state);
-    gpio_set_level(BLINK_GPIO_26, s_led_state);
-    gpio_set_level(BLINK_GPIO_27, s_led_state);
+    ESP_ERROR_CHECK(ledc_set_duty(LEDC_IO25_MODE, LEDC_IO25_CHANNEL,  s_led_state * dimming_value ) );
+    // Update duty to apply the new value
+    ESP_ERROR_CHECK(ledc_update_duty(LEDC_IO25_MODE, LEDC_IO25_CHANNEL));
+
+    ESP_ERROR_CHECK(ledc_set_duty(LEDC_IO26_MODE, LEDC_IO26_CHANNEL,  s_led_state * dimming_value ) );
+    // Update duty to apply the new value
+    ESP_ERROR_CHECK(ledc_update_duty(LEDC_IO26_MODE, LEDC_IO26_CHANNEL));
+
+    ESP_ERROR_CHECK(ledc_set_duty(LEDC_IO27_MODE, LEDC_IO27_CHANNEL,  s_led_state * dimming_value ) );
+    // Update duty to apply the new value
+    ESP_ERROR_CHECK(ledc_update_duty(LEDC_IO27_MODE, LEDC_IO27_CHANNEL));
+
     return 0;
 }
 
@@ -100,5 +187,22 @@ int lightbulb_set_hue(float value)
 int lightbulb_set_brightness(int value)
 {
     ESP_LOGI(TAG, "lightbulb_set_brightness : %d", value);
+    // Set duty to 50%
+    dimming_value = (( (value) * 8192 ) / 100);
+
+    ESP_LOGI(TAG, "dimming_value to led : %d", dimming_value);
+
+    ESP_ERROR_CHECK(ledc_set_duty(LEDC_IO25_MODE, LEDC_IO25_CHANNEL,  dimming_value ) );
+    // Update duty to apply the new value
+    ESP_ERROR_CHECK(ledc_update_duty(LEDC_IO25_MODE, LEDC_IO25_CHANNEL));
+
+    ESP_ERROR_CHECK(ledc_set_duty(LEDC_IO26_MODE, LEDC_IO26_CHANNEL,  dimming_value ) );
+    // Update duty to apply the new value
+    ESP_ERROR_CHECK(ledc_update_duty(LEDC_IO26_MODE, LEDC_IO26_CHANNEL));
+
+    ESP_ERROR_CHECK(ledc_set_duty(LEDC_IO27_MODE, LEDC_IO27_CHANNEL,  dimming_value ) );
+    // Update duty to apply the new value
+    ESP_ERROR_CHECK(ledc_update_duty(LEDC_IO27_MODE, LEDC_IO27_CHANNEL));
+
     return 0;
 }
